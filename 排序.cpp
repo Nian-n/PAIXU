04c873fabff71b9c9a48165af893edfeb5d5dd72
Nian-n <918417426@qq.com>
2019-10-22 20:54:10 +0800

    排序

---
+++ 排序.cpp
@@ -0,0 +1,35 @@
+//#include<iostream>
+//#include<stdlib.h>
+//#include<windows.h>
+//#include<string>
+//using namespace std;
+//int main()
+//{
+//	int tmp;
+//	int a[5];
+//	for (int k = 0; k < 5; k++)
+//	{
+//		cin >> a[k];
+//	}
+//	for (int i = 0; i < 4; i++)
+//	{
+//		for (int j = i; j < 4; j++)
+//		{
+//			if (a[i] > a[j + 1])
+//			{
+//				tmp = a[i];
+//				a[i] = a[j + 1];
+//				a[j + 1] = tmp;
+//			}
+//
+//		}
+//	}
+//		for (int k = 0; k < 5; k++)
+//		{
+//			cout << a[k] << ends;
+//		}
+//		
+//
+//	system("pause");
+//	return 0;
+//}
